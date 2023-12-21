dcf607f0df0b0b0e4368567bfaffb6f3d580686e
valiullin_fi <vfi@eleps.ru>
2023-12-21 17:31:11 +0300

    Добавил прерывание UART по приему

--- Src/main.c
+++ Src/main.c
@@ -12,9 +12,13 @@ void RTC_task(void *pvParam);
 const char *weekdayName[7] = {"Sunday\0", "Monday\0", "Tuesday\0", "Wednesday\0", "Thursday\0",
                               "Friday\0", "Saturday\0"};
 
+#define BUFFER_SIZE_TX 40
+#define BUFFER_SIZE_RX 2
+
 uint8_t red_g = 50, green_g = 50, blue_g = 50, display = 0;
-char buff[18], uart_buff[100];
-uint8_t ds1307_data[7], si7210_data[21] = {0};
+char buff[18], buff_uart_tx[BUFFER_SIZE_TX];
+uint8_t buff_uart_rx[BUFFER_SIZE_RX];
+uint8_t ds1307_data[7];
 
 int main() {
     __disable_irq();
@@ -23,12 +27,19 @@ int main() {
     IWDG_Init();
     UART1_Init();
     SSD1306_Init();
+
+    if (HAL_I2C_IsDeviceReady(&hi2c1, AT24CXX_I2C_ADDR, 3, 100) == HAL_OK) {
+        HAL_UART_Transmit_IT(&huart1, "EEPROM_IC ready!\n\r", 18);
+    } else {
+        HAL_UART_Transmit_IT(&huart1, "EEPROM_IC error connection!\n\r", 25);
+    }
+
     ARGB_PreInit();
     ARGB_Init();
     ARGB_SetBrightness(255);
     __enable_irq();
-    xTaskCreate(Buttons_Processing, "1", 128, NULL, 2, NULL);
-    xTaskCreate(RTC_task, "2", 128, NULL, 3, NULL);
+    xTaskCreate(Buttons_Processing, "3", 256, NULL, 2, NULL);
+    xTaskCreate(RTC_task, "3", 256, NULL, 3, NULL);
     vTaskStartScheduler();
     while (1) {}
 }
@@ -65,6 +76,12 @@ void Buttons_Processing(void *pvParam) {
             green_g = 255;
             blue_g = 0;
             HAL_UART_Transmit_IT(&huart1, "GREEN\n\r", 7);
+        }
+        if (BUTTON_GetAction(DOWN) == BUTTON_LONG_PRESS) {
+            red_g = 0;
+            green_g = 255;
+            blue_g = 255;
+            HAL_UART_Transmit_IT(&huart1, "AQUA\n\r", 6);
         }
         if (BUTTON_GetAction(LEFT) == BUTTON_SHORT_PRESS) {
             red_g = 0;
@@ -104,24 +121,25 @@ void Buttons_Processing(void *pvParam) {
 }
 
 void RTC_task(void *pvParam) {
+    uint16_t cnt = 0;
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
 
-    HAL_IWDG_Refresh(&hiwdg);
-    if (HAL_I2C_IsDeviceReady(&hi2c1, RTC_I2C_ADDR, 3, 100) == HAL_OK) {
-        HAL_UART_Transmit_IT(&huart1, "RTC IC ready!\n\r", 15);
-    } else {
-        HAL_UART_Transmit_IT(&huart1, "RTC IC error connection!\n\r", 22);
-    }
     CtrlRegCfg(1, 1, 1, 1, hi2c1);
     // SetDateTime(20, 4, 12, 23, 11, 32, 0, hi2c1); // Установка времени, закомментировать после использования
 
+    HAL_UART_Receive_IT(&huart1, buff_uart_rx, 1);
     while (1) {
+        HAL_IWDG_Refresh(&hiwdg);
         GetDateTime(ds1307_data, hi2c1);
-        sprintf(uart_buff, "%02d(%1d).%02d.%02d %02d:%02d:%02d\n\r", ds1307_data[4], ds1307_data[3], ds1307_data[5],
-                ds1307_data[6], ds1307_data[2], ds1307_data[1],
-                ds1307_data[0]);
-        HAL_UART_Transmit_IT(&huart1, uart_buff, 22);
+        if (++cnt == 300) {
+            sprintf(buff_uart_tx, "%02d(%1d).%02d.%02d %02d:%02d:%02d\n\r", ds1307_data[4], ds1307_data[3],
+                    ds1307_data[5],
+                    ds1307_data[6], ds1307_data[2], ds1307_data[1],
+                    ds1307_data[0]);
+            HAL_UART_Transmit_IT(&huart1, buff_uart_tx, 22);
+            cnt = 0;
+        }
         if (display == 0) {
             SSD1306_Fill(SSD1306_COLOR_BLACK);
             SSD1306_GotoXY(0, 0);
@@ -133,22 +151,40 @@ void RTC_task(void *pvParam) {
             SSD1306_GotoXY(0, 44);
             SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
             SSD1306_UpdateScreen();
-        }
-        else if (display == 1) {
+        } else if (display == 1) {
             SSD1306_Fill(SSD1306_COLOR_BLACK);
+            sprintf(buff, "R: %03d", red_g);
             SSD1306_GotoXY(0, 0);
-            SSD1306_Puts("PIZDEC", &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "NAXYI");
+            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+            sprintf(buff, "G: %03d", green_g);
             SSD1306_GotoXY(0, 22);
             SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "BLYAD");
+            sprintf(buff, "B: %03d", blue_g);
             SSD1306_GotoXY(0, 44);
             SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
             SSD1306_UpdateScreen();
-        }
-        else if (display == 2) {}
+        } else if (display == 2) {}
         GPIOC->ODR ^= GPIO_ODR_ODR13;
         vTaskDelayUntil(&xLastWakeTime, 1000);
     }
     vTaskDelete(NULL);
+}
+
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == USART1) {
+        if (buff_uart_rx[0] == 'r') {
+            red_g = 255;
+            green_g = 0;
+            blue_g = 0;
+        } else if (buff_uart_rx[0] == 'g') {
+            red_g = 0;
+            green_g = 255;
+            blue_g = 0;
+        } else if (buff_uart_rx[0] == 'b') {
+            red_g = 0;
+            green_g = 0;
+            blue_g = 255;
+        }
+        HAL_UART_Receive_IT(&huart1, buff_uart_rx, 1);
+    }
 }
--- Src/system.c
+++ Src/system.c
@@ -170,7 +170,7 @@ void UART1_Init() {
     __HAL_RCC_USART1_CLK_ENABLE();
 
     huart1.Instance = USART1;
-    huart1.Init.BaudRate = 9600;
+    huart1.Init.BaudRate = 115200;
     huart1.Init.WordLength = UART_WORDLENGTH_8B;
     huart1.Init.StopBits = UART_STOPBITS_1;
     huart1.Init.Parity = UART_PARITY_NONE;

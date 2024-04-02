701d9d2361924cdfb9c8bcbcac327485cd740e6d
valiullin_fi <vfi@eleps.ru>
2024-04-02 16:35:18 +0300

    рÐРешил warnings main.c

--- Src/main.c
+++ Src/main.c
@@ -6,13 +6,13 @@ extern UART_HandleTypeDef huart1, huart2;
 extern TIM_HandleTypeDef htim1, htim2, htim3, htim4;
 extern ADC_HandleTypeDef hadc1;
 
-void Buttons_task(void *pvParam);
+_Noreturn void Buttons_task();
 
-void RTC_task(void *pvParam);
+_Noreturn void RTC_task();
 
-void OLED_task(void *pvParam);
+_Noreturn void OLED_task();
 
-void ADC_task(void *pvParam);
+_Noreturn void ADC_task();
 
 const char *weekdayName[7] = {"Sunday\0", "Monday\0", "Tuesday\0", "Wednesday\0", "Thursday\0",
                               "Friday\0", "Saturday\0"};
@@ -24,7 +24,7 @@ uint8_t red_g = 50, green_g = 50, blue_g = 50, display = 2;
 char buff[18], buff_uart_tx[BUFFER_SIZE_TX];
 uint8_t buff_uart_rx[BUFFER_SIZE_RX];
 uint8_t ds1307_data[7];
-uint16_t adc1_in0 = 1703;
+uint32_t adc1_in0;
 
 int main() {
     __disable_irq();
@@ -34,9 +34,9 @@ int main() {
     UART1_Init();
     SSD1306_Init();
     if (HAL_I2C_IsDeviceReady(&hi2c1, AT24CXX_I2C_ADDR, 3, 100) == HAL_OK) {
-        HAL_UART_Transmit_IT(&huart1, "EEPROM_IC ready!\n\r", 18);
+        HAL_UART_Transmit_IT(&huart1, (uint8_t *) "EEPROM_IC ready!\n\r", 18);
     } else {
-        HAL_UART_Transmit_IT(&huart1, "EEPROM_IC error connection!\n\r", 25);
+        HAL_UART_Transmit_IT(&huart1, (uint8_t *) "EEPROM_IC error connection!\n\r", 25);
     }
 
     ARGB_PreInit();
@@ -48,12 +48,9 @@ int main() {
     xTaskCreate(OLED_task, "3", 256, NULL, 3, NULL);
     xTaskCreate(ADC_task, "4", 256, NULL, 2, NULL);
     vTaskStartScheduler();
-    while (1) {
-        HAL_Delay(1000);
-    }
 }
 
-void Buttons_task(void *pvParam) {
+_Noreturn void Buttons_task() {
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
 
@@ -66,55 +63,55 @@ void Buttons_task(void *pvParam) {
             red_g = 255;
             green_g = 0;
             blue_g = 0;
-            HAL_UART_Transmit_IT(&huart1, "RED\n\r", 5);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "RED\n\r", 5);
         }
         if (BUTTON_GetAction(UP) == BUTTON_LONG_PRESS) {
             red_g = 255;
             green_g = 0;
             blue_g = 255;
-            HAL_UART_Transmit_IT(&huart1, "PURPLE\n\r", 8);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "PURPLE\n\r", 8);
         }
         if (BUTTON_GetAction(UP) == BUTTON_VERY_LONG_PRESS) {
             red_g = 255;
             green_g = 255;
             blue_g = 0;
-            HAL_UART_Transmit_IT(&huart1, "YELLOW\n\r", 8);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "YELLOW\n\r", 8);
         }
         if (BUTTON_GetAction(DOWN) == BUTTON_SHORT_PRESS) {
             red_g = 0;
             green_g = 255;
             blue_g = 0;
-            HAL_UART_Transmit_IT(&huart1, "GREEN\n\r", 7);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "GREEN\n\r", 7);
         }
         if (BUTTON_GetAction(DOWN) == BUTTON_LONG_PRESS) {
             red_g = 0;
             green_g = 255;
             blue_g = 255;
-            HAL_UART_Transmit_IT(&huart1, "AQUA\n\r", 6);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "AQUA\n\r", 6);
         }
         if (BUTTON_GetAction(LEFT) == BUTTON_SHORT_PRESS) {
             red_g = 0;
             green_g = 0;
             blue_g = 255;
-            HAL_UART_Transmit_IT(&huart1, "BLUE\n\r", 6);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "BLUE\n\r", 6);
         }
         if (BUTTON_GetAction(RIGHT) == BUTTON_SHORT_PRESS) {
             red_g = 255;
             green_g = 255;
             blue_g = 255;
-            HAL_UART_Transmit_IT(&huart1, "WHITE\n\r", 7);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "WHITE\n\r", 7);
         }
         if (BUTTON_GetAction(MENU) == BUTTON_SHORT_PRESS) {
             red_g = 0;
             green_g = 0;
             blue_g = 0;
-            HAL_UART_Transmit_IT(&huart1, "CLEAR\n\r", 7);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "CLEAR\n\r", 7);
         }
         if (BUTTON_GetAction(REC) == BUTTON_SHORT_PRESS) {
             red_g = 0;
             green_g = 255;
             blue_g = 255;
-            HAL_UART_Transmit_IT(&huart1, "AQUA\n\r", 6);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) "AQUA\n\r", 6);
         }
         if (BUTTON_GetAction(PHOTO) == BUTTON_SHORT_PRESS) {
             if (++display > 2) {
@@ -126,10 +123,10 @@ void Buttons_task(void *pvParam) {
         BUTTON_ResetActions();
         vTaskDelayUntil(&xLastWakeTime, 1);
     }
-    vTaskDelete(NULL);
+    //vTaskDelete(NULL);
 }
 
-void RTC_task(void *pvParam) {
+_Noreturn void RTC_task() {
     uint16_t cnt = 0;
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
@@ -146,60 +143,67 @@ void RTC_task(void *pvParam) {
                     ds1307_data[5],
                     ds1307_data[6], ds1307_data[2], ds1307_data[1],
                     ds1307_data[0]);
-            HAL_UART_Transmit_IT(&huart1, buff_uart_tx, 22);
+            HAL_UART_Transmit_IT(&huart1, (uint8_t *) buff_uart_tx, 22);
             cnt = 0;
         }
         GPIOC->ODR ^= GPIO_ODR_ODR13;
         vTaskDelayUntil(&xLastWakeTime, 1000);
     }
-    vTaskDelete(NULL);
+    //vTaskDelete(NULL);
 }
 
-void OLED_task(void *pvParam) {
+_Noreturn void OLED_task() {
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
 
     while (1) {
         HAL_IWDG_Refresh(&hiwdg);
-        if (display == 0) {
-            SSD1306_Fill(SSD1306_COLOR_BLACK);
-            SSD1306_GotoXY(0, 0);
-            SSD1306_Puts((char *) weekdayName[ds1307_data[3] - 1], &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "%02d.%02d.%02d", ds1307_data[4], ds1307_data[5], ds1307_data[6]);
-            SSD1306_GotoXY(0, 22);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "%02d:%02d:%02d", ds1307_data[2], ds1307_data[1], ds1307_data[0]);
-            SSD1306_GotoXY(0, 44);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            SSD1306_UpdateScreen();
-        } else if (display == 1) {
-            SSD1306_Fill(SSD1306_COLOR_BLACK);
-            sprintf(buff, "R: %03d", red_g);
-            SSD1306_GotoXY(0, 0);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "G: %03d", green_g);
-            SSD1306_GotoXY(0, 22);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "B: %03d", blue_g);
-            SSD1306_GotoXY(0, 44);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            SSD1306_UpdateScreen();
-        } else if (display == 2) {
-            SSD1306_Fill(SSD1306_COLOR_BLACK);
-            sprintf(buff, "ADC1_IN0:");
-            SSD1306_GotoXY(0, 0);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            sprintf(buff, "%04d", adc1_in0);
-            SSD1306_GotoXY(0, 22);
-            SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
-            SSD1306_UpdateScreen();
+        switch (display) {
+            case 0:
+                SSD1306_Fill(SSD1306_COLOR_BLACK);
+                SSD1306_GotoXY(0, 0);
+                SSD1306_Puts((char *) weekdayName[ds1307_data[3] - 1], &Font_11x18, SSD1306_COLOR_WHITE);
+                sprintf(buff, "%02d.%02d.%02d", ds1307_data[4], ds1307_data[5], ds1307_data[6]);
+                SSD1306_GotoXY(0, 22);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                sprintf(buff, "%02d:%02d:%02d", ds1307_data[2], ds1307_data[1], ds1307_data[0]);
+                SSD1306_GotoXY(0, 44);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                SSD1306_UpdateScreen();
+                break;
+            case 1:
+                SSD1306_Fill(SSD1306_COLOR_BLACK);
+                sprintf(buff, "R: %03d", red_g);
+                SSD1306_GotoXY(0, 0);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                sprintf(buff, "G: %03d", green_g);
+                SSD1306_GotoXY(0, 22);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                sprintf(buff, "B: %03d", blue_g);
+                SSD1306_GotoXY(0, 44);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                SSD1306_UpdateScreen();
+                break;
+            case 2:
+                SSD1306_Fill(SSD1306_COLOR_BLACK);
+                sprintf(buff, "ADC1_IN0:");
+                SSD1306_GotoXY(0, 0);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                sprintf(buff, "%04d", adc1_in0);
+                SSD1306_GotoXY(0, 22);
+                SSD1306_Puts(buff, &Font_11x18, SSD1306_COLOR_WHITE);
+                SSD1306_UpdateScreen();
+                break;
+            default :
+                break;
+
         }
         vTaskDelayUntil(&xLastWakeTime, 250);
     }
-    vTaskDelete(NULL);
+    //vTaskDelete(NULL);
 }
 
-void ADC_task(void *pvParam) {
+_Noreturn void ADC_task() {
     portTickType xLastWakeTime;
     xLastWakeTime = xTaskGetTickCount();
 
@@ -207,10 +211,10 @@ void ADC_task(void *pvParam) {
 
     while (1) {
         HAL_IWDG_Refresh(&hiwdg);
-        HAL_ADC_Start_DMA(&hadc1, (uint32_t *) adc1_in0, 1);
+        HAL_ADC_Start_DMA(&hadc1, &adc1_in0, 1);
         vTaskDelayUntil(&xLastWakeTime, 500);
     }
-    vTaskDelete(NULL);
+    //vTaskDelete(NULL);
 }
 
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
@@ -233,5 +237,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
 }
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc) {
-    if (hadc->Instance == ADC1) {}
+    if (hadc->Instance == ADC1) {
+
+    }
 }
